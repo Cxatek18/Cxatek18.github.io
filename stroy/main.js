948bc938097fcc5098cefc1b930cbeec253c5008
Cxatek18 <76473073+Cxatek18@users.noreply.github.com>
2021-07-10 23:25:28 +0500

    Delete stroy directory

--- stroy/main.js
+++
@@ -1,7 +0,0 @@
-const   open = document.querySelector('.burger');
-        menu = document.querySelector('.menu');
-
-open.onclick = function () {
-    open.classList.toggle('burger_active')
-    menu.classList.toggle('visible')
-}
