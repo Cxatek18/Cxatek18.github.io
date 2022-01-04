b666850792bd3addb6e57ab1cb29ab8338950f14
Cxatek18 <76473073+Cxatek18@users.noreply.github.com>
2022-01-04 14:36:10 +0500

    Add files via upload

---
+++ test_website/telegram.php
@@ -0,0 +1,34 @@
+<?php
+
+/* https://api.telegram.org/bot2017681643:AAGIV8PiMTxMXsQymHoobvY06PZd25nuEnw/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$token = "2017681643:AAGIV8PiMTxMXsQymHoobvY06PZd25nuEnw";
+$chat_id = "-629803427";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+if (mb_strlen($name, 'utf-8') > 25){
+  echo "Please enter"
+}else {
+  $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+}
+
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+
+?>
